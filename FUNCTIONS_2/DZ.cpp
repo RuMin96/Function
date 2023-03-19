619ae9a93be1ea5dda1bb75e5cbe19415044f674
PC <PC@DESKTOP-OR1E14N>
2023-03-19 17:33:38 +0700

    ENCODING

---
+++ FUNCTIONS_2/DZ.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+
+
+void main()
+{
+	int i;
+	int arr[10];
+	srand(time(NULL));
+	for (i = 0; i < 10; i++);
+	{
+		arr[i] = (1+ rand() % 100);
+		cout << arr[i] << "\n";
+	}
+	return;
+}
